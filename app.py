333f7348b54d105193ed36bdc9524781bd705ddb
Diana <dianitais@gmail.com>
2024-08-29 11:23:05 -0500

    app

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Análisis de datos de vehículos')
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+
+
+# Casilla de verificación para el histograma
+if st.checkbox('Mostrar histograma'):
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+# Casilla de verificación para el gráfico de dispersión
+if st.checkbox('Mostrar gráfico de dispersión'):
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
