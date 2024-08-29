33b80cad5358c036998d94503b54fdf5fe167833
Diana <dianitais@gmail.com>
2024-08-29 11:47:31 -0500

    add app.py

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 
 # Casilla de verificación para el histograma
-if st.checkbox('Mostrar histograma'):
+build_histogram = st.checkbox('Construir un histograma')
+if build_histogram:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
 # Casilla de verificación para el gráfico de dispersión
-if st.checkbox('Mostrar gráfico de dispersión'):
+build_scatter_plot = st.checkbox('Mostrar gráfico de dispersión')
+if build_scatter_plot:
     st.write(
         'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
